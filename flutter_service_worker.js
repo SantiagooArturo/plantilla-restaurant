88309769e5e483763449ec3615aabab6100b1696
SantiagooArturo <sfrancobaanante@gmail.com>
2025-08-07 14:49:38 -0500

    Move Flutter web files to root for Vercel deployment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5cec4deb6de06a2aec92e4841589b9bb",
+const RESOURCES = {"flutter_bootstrap.js": "ffb7c2850235072d00d780efbeb62d29",
 "version.json": "a4f4bf9f2d496a21446cf13e0db86b8b",
 "index.html": "e68c26583fbee1484e1bfa985c3b3ab6",
 "/": "e68c26583fbee1484e1bfa985c3b3ab6",
